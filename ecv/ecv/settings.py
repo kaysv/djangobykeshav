e6cd4e70e92770274dd99d5fc06c54a5c5828d6f
keshav <bkesha.anaar@gmail.com>
2017-12-17 21:12:06 +0545

    basic settings and first migrations

--- ecv/ecv/settings.py
+++ ecv/ecv/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ecv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(
+    os.path.dirname(BASE_DIR), "static_out_project", "static_root")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
